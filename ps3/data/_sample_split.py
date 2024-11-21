7c7d15766606c8ffcb526be8acb0cbee2b07f4d8
Gengchen0326 <450030858@qq.com>
2024-11-21 21:35:44 +0000

    E1 AND E2

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,25 +1,32 @@
+import pandas as pd
 import hashlib
 
-import numpy as np
-
-# TODO: Write a function which creates a sample split based in some id_column and training_frac.
-# Optional: If the dtype of id_column is a string, we can use hashlib to get an integer representation.
-def create_sample_split(df, id_column, training_frac=0.8):
-    """Create sample split based on ID column.
+def create_sample_split(df: pd.DataFrame, id_column: str, training_frac: float = 0.8) -> pd.DataFrame:
+    """
+    Create a sample split column based on the specified ID column.
 
     Parameters
     ----------
     df : pd.DataFrame
-        Training data
+        The input dataframe.
     id_column : str
-        Name of ID column
+        Name of the ID column to base the split on.
     training_frac : float, optional
-        Fraction to use for training, by default 0.9
+        Fraction of data to assign to the training set, by default 0.8.
 
     Returns
     -------
     pd.DataFrame
-        Training data with sample column containing train/test split based on IDs.
+        The dataframe with an additional column 'sample' indicating 'train' or 'test'.
     """
+    # Ensure the ID column is of string type for hashing
+    id_series = df[id_column].astype(str)
 
+    # Use hashlib to generate deterministic integer values from the IDs
+    hash_values = id_series.apply(lambda x: int(hashlib.md5(x.encode()).hexdigest(), 16) % 100)
+
+    # Assign rows to 'train' or 'test' based on the hash value and training fraction
+    df['sample'] = hash_values.apply(lambda x: 'train' if x < training_frac * 100 else 'test')
+    
     return df
+
